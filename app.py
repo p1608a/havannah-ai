1c84baf09329f9d0e25a626ef1017258bdf1a695
Pawan Kumar <pawankr27122001@gmail.com>
2026-06-10 09:04:21 +0530

    Initial commit

--- app.py
+++ app.py
@@ -94,5 +94,7 @@ def make_move():
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
